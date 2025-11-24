ae4db2893ab673ac25fe57b1a7d1c6229fb9802a
Akshaykam_Official <72594492+akshaykam@users.noreply.github.com>
2025-11-24 18:18:24 +0530

    Update service URLs to production endpoints
    updating temporary IP of EC2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  movieServiceUrl: 'http://localhost:8081/api/v1',
-  reviewServiceUrl: 'http://localhost:8082/api/v1',
+  movieServiceUrl: 'http://13.221.219.76:8081/api/v1',
+  reviewServiceUrl: 'http://13.221.219.76:8082/api/v1',
   enableTracing: true
 };
