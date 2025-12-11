33f8249ec4d9dcff447682c52e434fd9e213e161
akshaykam <akshaykam@cybage.com>
2025-12-11 15:19:08 +0530

    changes to deploy backend1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  movieServiceUrl: 'http://http://ab232449873264c178b73f71698d0029-2059333677.us-east-1.elb.amazonaws.com:8081/api/v1',
-  reviewServiceUrl: 'http://http://ab232449873264c178b73f71698d0029-2059333677.us-east-1.elb.amazonaws.com:8082/api/v1',
+  movieServiceUrl: 'http://ab232449873264c178b73f71698d0029-2059333677.us-east-1.elb.amazonaws.com:8081/api/v1',
+  reviewServiceUrl: 'http://ab232449873264c178b73f71698d0029-2059333677.us-east-1.elb.amazonaws.com:8082/api/v1',
   enableTracing: true
 };
