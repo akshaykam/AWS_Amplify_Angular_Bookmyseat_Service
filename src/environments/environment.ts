2a91675b292f4b494057b21817ebaf3a46ab168b
akshaykam <akshaykam@cybage.com>
2025-12-11 15:10:00 +0530

    changes to deploy backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   movieServiceUrl: 'http://13.221.219.76:8081/api/v1',
   reviewServiceUrl: 'http://13.221.219.76:8082/api/v1',
   enableTracing: true
